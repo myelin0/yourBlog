87e85187822c1ced48c35736abea1880a5b1757e
myelin0 <emi.veisllari@gmail.com>
2022-09-23 18:33:26 +0200

    creating associations between models

--- db/migrate/20220915131605_create_posts.rb
+++ db/migrate/20220915131605_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references:author, null: false,foreign_key:{to_table: :users}, index: true
+      t.references:user, null: false,foreign_key:{to_table: :users}, index: true
       t.string :title
       t.integer :comments_counter
       t.integer :likes_counter
