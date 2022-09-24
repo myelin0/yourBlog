8cf0e03fca4c1c9e326e662459810bea2ac77814
myelin0 <emi.veisllari@gmail.com>
2022-09-24 15:11:35 +0200

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 250 }
   validates :CommentsCounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :LikesCounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
- 
+
   after_save :update_post_counter
   def recent_comments
     comments.limit(5).order(created_at: :desc)
