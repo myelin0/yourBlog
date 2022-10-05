36fef06e5a8a7677617ff6cc6cc64ffa45cc2935
myelin0 <emi.veisllari@gmail.com>
2022-10-05 14:28:24 +0200

    Add integration tests

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -11,14 +11,14 @@ def testgroup
     expect(page).to have_content('Hi Raef!')
   end
   it 'display the how many comments' do
-    expect(page).to have_content("#{@first_post.comments_counter} comment")
+    expect(page).to have_content("#{@first_post.CommentsCounter} comment")
   end
   it 'display the how many Likes' do
-    expect(page).to have_content("#{@first_post.likes_counter} like")
+    expect(page).to have_content("#{@first_post.LikesCounter} like")
   end
   it 'after clicking on a post, redirect on a post show page' do
     click_link(@first_post.title)
-    expect(page).to have_content("#{@first_post.comments_counter} comment")
+    expect(page).to have_content("#{@first_post.CommentsCounter} comment")
   end
 end
 
@@ -28,7 +28,7 @@ RSpec.feature 'Post Index Page', type: :feature do
     @user2 = User.create(name: 'Negi', photo: 'https://images.unsplash.com/ph=80', bio: 'Teacher from Egypt.')
     @first_post = Post.create(user: @user, title: 'Hello', text: 'This is my first post')
     5.times do |_i|
-      Comment.create(post: @first_post, user: @user2, text: 'Hi Emi!')
+      Comment.create(post: @first_post, user: @user2, text: 'Hi Raef!')
     end
     visit user_posts_path(@user)
   end
--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 RSpec.feature 'User Index Page', type: :feature do
   before :each do
-    @user = User.create(name: 'Emi',
-                        photo: 'https://images.unsplash.com/photo-q=80', bio: 'Teacher from Albania.')
-    @user2 = User.create(name: 'Negi',
-                         photo: 'https://images.unsplash.com/photo-q=80', bio: 'Teacher from Egypt.')
+    @user_1 = User.create(name: 'Emi',
+                        photo: 'https://images.unsplash.com/photo-q=80', bio: 'Teacher from Albania.', PostsCounter: 0)
+    @user_2 = User.create(name: 'Negi',
+                         photo: 'https://images.unsplash.com/photo-q=80', bio: 'Teacher from Egypt.', PostsCounter: 0)
 
-    visit user_path(@user)
+    visit user_path(@user_1)
   end
   it ' displays the username for all users' do
     expect(page).to have_content('Emi')
   end
 
   it 'I can see the profile picture for each user' do
-    expect(page.find(:css, '.user_page_image')[:src]).to have_content(@user.photo)
+    expect(page.find(:css, '.user_page_image')[:src]).to have_content(@user_1.photo)
   end
 
   it 'should display the number of posts each user has written.' do
@@ -21,7 +21,7 @@ RSpec.feature 'User Index Page', type: :feature do
   end
 
   it " When I click on a user it should redirected to that user's show page" do
-    visit(user_path(@user.id))
+    visit(user_path(@user_1.id))
     expect(page).to have_content('Bio')
   end
 end
